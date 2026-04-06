3fbd75e27b138a898b730fd6ed316f7c497cec5b
Mahmoud <mahmoudadl206gmail.com>
2026-04-06 23:13:15 +0200

    tp-3-V2

--- script.js
+++ script.js
@@ -16,11 +16,18 @@ const deltaplanes = [
     { id: 3, src: "images/gliders/SuperGliderMK8.png" }
 ];
 
+// Matrice des prévisualisations : Ligne = Carrosserie, Colonne = Roues
+const previewMatrix = [
+    ["images/previews/IMG_7604.png", "images/previews/IMG_7602.png", "images/previews/IMG_7603.png"],
+    ["images/previews/IMG_7607.png", "images/previews/IMG_7605.png", "images/previews/IMG_7606.png"],
+    ["images/previews/IMG_7610.png", "images/previews/IMG_7608.png", "images/previews/IMG_7609.png"]
+];
+
 class Carousel {
-    constructor(data, previewId) {
+    constructor(data, onChange) {
         this.data = data;
         this.currentIndex = 0;
-        this.previewId = previewId;
+        this.onChange = onChange; // Fonction appelée à chaque changement
         this.initDOM();
         this.updateView();
     }
@@ -31,53 +38,90 @@ class Carousel {
         this.wrapper = document.createElement("div");
         this.wrapper.className = "carousel";
 
+        // Image estompée (haut)
+        this.topImg = document.createElement("img");
+        this.topImg.className = "faded-img";
+
+        // Bouton Haut
         this.btnUp = document.createElement("button");
         this.btnUp.className = "carousel-btn";
         const imgUp = document.createElement("img");
-        imgUp.src = "images/arrow.png"; 
+        imgUp.src = "images/arrow.png";
         this.btnUp.appendChild(imgUp);
         this.btnUp.addEventListener("click", () => this.prev());
 
-        this.imgElement = document.createElement("img");
-        this.imgElement.className = "carousel-img";
+        // Conteneur Actif (Bordure jaune)
+        this.activeBox = document.createElement("div");
+        this.activeBox.className = "active-box";
+        this.activeImg = document.createElement("img");
+        this.activeImg.className = "carousel-img";
+        this.activeBox.appendChild(this.activeImg);
 
+        // Bouton Bas
         this.btnDown = document.createElement("button");
         this.btnDown.className = "carousel-btn btn-down";
         const imgDown = document.createElement("img");
-        imgDown.src = "images/arrow.png"; 
+        imgDown.src = "images/arrow.png";
         this.btnDown.appendChild(imgDown);
         this.btnDown.addEventListener("click", () => this.next());
 
+        // Image estompée (bas)
+        this.bottomImg = document.createElement("img");
+        this.bottomImg.className = "faded-img";
+
+        // Assemblage
+        this.wrapper.appendChild(this.topImg);
         this.wrapper.appendChild(this.btnUp);
-        this.wrapper.appendChild(this.imgElement);
+        this.wrapper.appendChild(this.activeBox);
         this.wrapper.appendChild(this.btnDown);
+        this.wrapper.appendChild(this.bottomImg);
 
         container.appendChild(this.wrapper);
     }
 
+    getPrevIndex() {
+        return (this.currentIndex - 1 + this.data.length) % this.data.length;
+    }
+
+    getNextIndex() {
+        return (this.currentIndex + 1) % this.data.length;
+    }
+
     updateView() {
-        const currentItem = this.data[this.currentIndex];
-        this.imgElement.src = currentItem.src;
-        document.getElementById(this.previewId).src = currentItem.src;
+        this.activeImg.src = this.data[this.currentIndex].src;
+        this.topImg.src = this.data[this.getPrevIndex()].src;
+        this.bottomImg.src = this.data[this.getNextIndex()].src;
+        
+        // On avertit le système global qu'un changement a eu lieu
+        if (this.onChange) {
+            this.onChange();
+        }
     }
 
     next() {
-        this.currentIndex++;
-        if (this.currentIndex >= this.data.length) {
-            this.currentIndex = 0;
-        }
+        this.currentIndex = this.getNextIndex();
         this.updateView();
     }
 
     prev() {
-        this.currentIndex--;
-        if (this.currentIndex < 0) {
-            this.currentIndex = this.data.length - 1;
-        }
+        this.currentIndex = this.getPrevIndex();
         this.updateView();
     }
 }
 
-new Carousel(karts, "preview-body");
-new Carousel(roues, "preview-tires");
-new Carousel(deltaplanes, "preview-glider");
+// Fonction globale pour mettre à jour l'image de Mario
+const updateMainPreview = () => {
+    const bodyIdx = kartCarousel.currentIndex;
+    const tireIdx = wheelCarousel.currentIndex;
+    
+    // On va chercher l'image correspondante dans la matrice 2D
+    document.getElementById("main-preview").src = previewMatrix[bodyIdx][tireIdx];
+};
+
+// Création des 3 instances
+const kartCarousel = new Carousel(karts, updateMainPreview);
+const wheelCarousel = new Carousel(roues, updateMainPreview);
+const gliderCarousel = new Carousel(deltaplanes, updateMainPreview);
+
+// Premier affichage
+updateMainPreview();
